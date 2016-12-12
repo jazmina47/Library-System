59916b5e9acd019a701572b9d411bab6747ba642
jazmina47 <jazminm0477@gmail>
2016-12-12 14:59:26 -0600

    added seeds

--- Library_ROR/Library_System/app/models/checkout.rb
+++ Library_ROR/Library_System/app/models/checkout.rb
@@ -1,6 +1,7 @@
 class Checkout < ApplicationRecord
 
 	has_and_belongs_to_many :materials
+	
 	validates_uniqueness_of :checkout_id
 
 
--- Library_ROR/Library_System/db/seeds.rb
+++ Library_ROR/Library_System/db/seeds.rb
@@ -16,26 +16,50 @@
 
 #cardholder_accounts ------------------------ 5 seeds
 
-#cardholder_accounts.create!(first_name: '', last_name: , fines: );
-
+CardholderAccount.create!(firstname: 'William', lastname: 'Qubert', cardholder_id: 1001)
+CardholderAccount.create!(firstname: 'Garry', lastname: 'Wells', cardholder_id: 1002)
+CardholderAccount.create!(firstname: 'Lizzy', lastname: 'Flor', cardholder_id: 1003)
+CardholderAccount.create!(firstname: 'Paul', lastname: 'Wilbert', cardholder_id: 1004)
+CardholderAccount.create!(firstname: 'Ollie', lastname: 'Revere', cardholder_id: 1005)
 
 #cardholder_personal_infos ------------------ 5 seeds
 
+#CardholderPersonalInfo.create!(name: 'Jazmin', lastname: 'Andrade', email: 'jmaya01@aurora.edu', phone_num: '340-234-1123', home_num: '2342343453', street: '123 Lake St.', city: 'Aurora', state: 'IL', zip: '60472', cardholder_id: 1001)
+#CardholderPersonalInfo.create!(name: 'Garry', lastname: 'Wells', email: 'gwells01@aurora.edu', phone_num: '234-214-1234', home_num: '3241232342', street: '343 Lake St.', city: 'Aurora', state: 'IL', zip: '60472', cardholder_id: 1002)
+#CardholderPersonalInfo.create!(name: 'Lizzy', lastname: 'Flor', email: 'lflor01@aurora.edu', phone_num: '345-234-5671', home_num: '6974542511', street: '445 Lake St.', city: 'Aurora', state: 'IL', zip: '60472', cardholder_id: 1003)
+#CardholderPersonalInfo.create!(name: 'Paul', lastname: 'Wilbert', email: 'pwilbert01@aurora.edu', phone_num: '213-234-4501', home_num: '4555653452', street: '154 Lake St.', city: 'Aurora', state: 'IL', zip: '60472', cardholder_id: 1004)
+#CardholderPersonalInfo.create!(name: 'Ollie', lastname: 'Revere', email: 'orevere01@aurora.edu', phone_num: '345-345-3243', home_num: '4563455463', street: '984 Lake St.', city: 'Aurora', state: 'IL', zip: '60472', cardholder_id: 1005)
 
 
 #checkouts ---------------------------------- 5 seeds
 
-
-
+#Checkout.create!(date: '2015, 12, 12', due_date: '2015, 01, 01', cardholder_id: '1', staff_member_id: '4')
+#Checkout.create!(date: '2015, 05, 04', due_date: '2015, 05, 30', cardholder_id: '2', staff_member_id: '5')
+#Checkout.create!(date: '2015, 06, 05', due_date: '2015, 06, 24', cardholder_id: '3', staff_member_id: '4')
+#Checkout.create!(date: '2015, 04, 03', due_date: '2015, 04, 15', cardholder_id: '4', staff_member_id: '5')
+#Checkout.create!(date: '2015, 06, 10', due_date: '2015, 06, 26', cardholder_id: '5', staff_member_id: '4')
 
 #fines -------------------------------------- 5 seeds
 
-
-
+#Fine.create!(date_of_fine: '2015, 01, 01', amount: '2.00', cardholder_id: '1')
+#Fine.create!(date_of_fine: '2015, 05, 30', amount: '3.50', cardholder_id: '2')
+#Fine.create!(date_of_fine: '2015, 06, 24', amount: '4.50', cardholder_id: '3')
+#Fine.create!(date_of_fine: '2015, 04, 15', amount: '3.00', cardholder_id: '4')
+#Fine.create!(date_of_fine: '2015, 06, 26', amount: '1.25', cardholder_id: '5')
 
 #materials ---------------------------------- 5 seeds
 
+#Material.create!(material_type: 'Book', material_classification_id: 'Fiction', material_title: 'Java 3 Introduction to Programming', quantity_material: '2')
+#Material.create!(material_type: 'Book', material_classification_id: 'Non-Fiction', material_title: 'Little Red Ridding Hood', quantity_material: '1')
+#Material.create!(material_type: 'Book', material_classification_id: 'Fiction', material_title: 'C++ Introduction to Programming', quantity_material: '5')
+#Material.create!(material_type: 'Book', material_classification_id: 'Fiction', material_title: 'This is Another Book', quantity_material: '3')
+#Material.create!(material_type: 'Book', material_classification_id: 'Non-Fiction', material_title: 'Astronomy: Volume 1', quantity_material: '1')
 
 
 
-#users -------------------------------------- 5 seeds
+
+
+
+
+
+
